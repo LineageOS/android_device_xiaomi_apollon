903000f54ad2c1b82d0d5c31091025c46e1c0670
Ramii Ahmed <ramy@ahmedramy.com>
2021-02-20 00:18:23 +0100

    apollo: Decommonize device-specific audio configs
    Change-Id: I6324d8d2e7f056e92f20524f58aa7760d3d2b330

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
