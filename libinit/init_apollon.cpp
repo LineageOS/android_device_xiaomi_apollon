a3abd271308ab331e321b54eab5a714514a7c970
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:36:07 +0200

    apollon: Add libinit for variants handling
    Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

---
+++ libinit/init_apollon.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_kona.h>
+
+static const variant_info_t apollocn_info = {
+    .prop_key = "ro.boot.hwc",
+    .prop_value = "CN",
+
+    .brand = "Redmi",
+    .device = "apollo",
+    .marketname = "K30S Ultra",
+    .model = "M2007J3SC",
+    .build_description = "apollo-user 11 RKQ1.200826.002 V12.5.1.0.RJDCNXM release-keys",
+    .build_fingerprint = "Redmi/apollo/apollo:11/RKQ1.200826.002/V12.5.1.0.RJDCNXM:user/release-keys",
+};
+
+static const variant_info_t apollo_info = {
+    .prop_key = "ro.boot.product.hardware.sku",
+    .prop_value = "std",
+
+    .brand = "Xiaomi",
+    .device = "apollo",
+    .marketname = "Mi 10T",
+    .model = "M2007J3SY",
+    .build_description = "apollo-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys",
+    .build_fingerprint = "Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys",
+};
+
+static const variant_info_t apollopro_info = {
+    .prop_key = "ro.boot.product.hardware.sku",
+    .prop_value = "pro",
+
+    .brand = "Xiaomi",
+    .device = "apollo",
+    .marketname = "Mi 10T Pro",
+    .model = "M2007J3SG",
+    .build_description = "apollopro-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys",
+    .build_fingerprint = "Xiaomi/apollopro_global/apollopro:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys",
+};
+
+static const std::vector<variant_info_t> variants = {
+    apollocn_info,
+    apollo_info,
+    apollopro_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+}
