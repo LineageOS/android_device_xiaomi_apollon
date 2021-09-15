5abeae44a88cee07cf4514f92aaadea86921b090
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-15 12:09:31 +0200

    apollon: Move common tree and shipping product API prop to the top
    Change-Id: I6fe21526ece548b49ab6891d99abb0057a304f02

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sm8250-common
+$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -20,11 +25,6 @@ PRODUCT_COPY_FILES += \
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
-
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Inherit from sm8250-common
-$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/apollon/apollon-vendor.mk)
