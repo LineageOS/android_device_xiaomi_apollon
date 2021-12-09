e7c127d10bbf53f837b72987634ab7c20e7dd1ca
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 20:24:28 +0100

    apollon: Namespace the tree
    Change-Id: I4642cbdff50e5f43e35dffb726565a829a8ccada

--- device.mk
+++ device.mk
@@ -26,5 +26,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/apollon/apollon-vendor.mk)
