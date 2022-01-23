e4d122db75344049b0405a9e4aa8e02517d6054a
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-23 22:45:44 +0200

    apollon: override releasetools extensions
    Change-Id: Ie728f2ada043d3533edefa9a553fba4f60f43fcb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ TARGET_OTA_ASSERT_DEVICE := apollon,apollo,apollopro
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/apollon/BoardConfigVendor.mk
