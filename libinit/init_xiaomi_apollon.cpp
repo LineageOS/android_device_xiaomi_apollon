f28a7aedffafdab38509ef67da2b8af4c0a27c79
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-29 16:38:50 +0200

    apollon: Update blobs from V12.5.10.0.RJDMIXM
    Change-Id: I3d061d287d3d7c9cf2584524789d0aebf4426e8c

--- libinit/init_xiaomi_apollon.cpp
+++ libinit/init_xiaomi_apollon.cpp
@@ -9,19 +9,6 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t apollocn_info = {
-    .hwc_value = "CN",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "apollo",
-    .marketname = "K30S Ultra",
-    .model = "M2007J3SC",
-    .build_fingerprint = "Redmi/apollo/apollo:11/RKQ1.200826.002/V12.5.7.0.RJDCNXM:user/release-keys",
-
-    .nfc = true,
-};
-
 static const variant_info_t apollo_global_info = {
     .hwc_value = "",
     .sku_value = "std",
@@ -30,7 +17,7 @@ static const variant_info_t apollo_global_info = {
     .device = "apollo",
     .marketname = "Mi 10T",
     .model = "M2007J3SY",
-    .build_fingerprint = "Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.4.0.RJDMIXM:user/release-keys",
+    .build_fingerprint = "Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.10.0.RJDMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -43,7 +30,7 @@ static const variant_info_t apollopro_info = {
     .device = "apollo",
     .marketname = "Mi 10T Pro",
     .model = "M2007J3SG",
-    .build_fingerprint = "Xiaomi/apollopro_global/apollo:11/RKQ1.200826.002/V12.5.5.0.RJDMIXM:user/release-keys",
+    .build_fingerprint = "Xiaomi/apollopro_global/apollo:11/RKQ1.200826.002/V12.5.10.0.RJDMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -56,13 +43,12 @@ static const variant_info_t apollo_info = {
     .device = "apollo",
     .marketname = "Redmi K30S Ultra",
     .model = "M2007J3SC",
-    .build_fingerprint = "Redmi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.5.0.RJDMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.10.0.RJDMIXM:user/release-keys",
 
     .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
-    apollocn_info,
     apollo_global_info,
     apollopro_info,
     apollo_info,
