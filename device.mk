636b57c1ced1da9131d3b1f34d70f74111dfd4b4
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-12 18:31:29 +0200

    apollon: Convert overlays to RRO
    Change-Id: Id0226559b81e115a6340591e7a107d25d7ad6848

--- device.mk
+++ device.mk
@@ -30,8 +30,11 @@ $(call soong_config_set,xiaomi_kona,variant_lib,//$(LOCAL_PATH):libvariant_xiaom
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    ApertureResApollon \
+    FrameworksResApollon \
+    LineageSDKResApollon \
+    SettingsResApollon \
+    SystemUIResApollon
 
 # QDCM
 PRODUCT_COPY_FILES += \
