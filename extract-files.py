3c6494fc6ebd5dd6e274d40e56d173dfe462f847
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    apollon: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -24,7 +24,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .add_line_if_missing('LEGACY_MIFARE_READER=1'),
+    'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),  
     'vendor/lib/hw/audio.primary.apollon.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so')
         .binary_regex_replace(
             b'/vendor/lib/liba2dpoffload.so',
             b'liba2dpoffload_apollon.so\x00\x00\x00\x00',
